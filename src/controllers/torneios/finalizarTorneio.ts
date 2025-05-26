d4435ebe2e56c89d08eeda01a5db95e6b041f8fb
Vinicius <vinicius_vnk@hotmail.com>
2025-05-26 18:13:26 -0300

    Ajuste na rota de finalizar torneio

--- src/controllers/torneios/finalizarTorneio.ts
+++ src/controllers/torneios/finalizarTorneio.ts
@@ -6,10 +6,13 @@ export class FinalizarTorneioUseCase{
     async execute({ id_torneio, resultados }: FinalizarTorneioDTO): Promise<any>{
 
         // Busca o campeonato caso seja do criador
-        let torneio = await prisma.torneios.findUnique({
-            where: {
-                id: id_torneio
+        const torneio = await prisma.torneios.findUnique({
+            where: { id: id_torneio },
+            select: {
+                dataFim: true,
+                status: true
             }
+
         });
 
         console.log("\n\nResposta: ");
@@ -19,7 +22,7 @@ export class FinalizarTorneioUseCase{
             throw new AppError('Torneio não encontrado');
         }
 
-        if( torneio.id_status !== Number(process.env.STATUS_JOGOS_FINALIZADOS) ){
+        if( torneio.status.id !== Number(process.env.STATUS_JOGOS_FINALIZADOS) ){
             console.log("Jogos não finalizados");
             throw new AppError('Jogos não finalizados');
         }
@@ -43,16 +46,17 @@ export class FinalizarTorneioUseCase{
 
 
         // Atualiza o status do torneio
-        await prisma.torneios.update({
+        const torneioStatus = await prisma.torneios.update({
             where: { id: id_torneio },
             data: {
                 id_status: Number(process.env.STATUS_FINALIZADO),
                 dataFim: torneio.dataFim < new Date() ? new Date() : torneio.dataFim,
-            }
+            },
+            select: { status: true }
         });
         
         console.log(`Torneio finalizado com sucesso \n${resultados.length} resultados adicionados com sucesso`);
 
-        return {};
+        return { torneio: torneioStatus }
     }
 }
